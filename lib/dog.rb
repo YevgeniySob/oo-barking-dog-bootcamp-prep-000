7aa205c9a27d6102aad62d808cdd177db30aa79e
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-06 02:56:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 class Dog
+  def initialize(name,breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+  
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
